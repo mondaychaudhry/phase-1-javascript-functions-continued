441da7ea76288fd57431209277edb8f52bd9e6e0
Monday <sobaata@gmail.com>
2021-07-20 23:31:45 -0400

    Completed.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Your code here
+function saturdayFun (activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork (activity = 'go to the office' ) {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective (a='*') {
+    let inner = function(adj='special') {
+        return `You are ${a}${adj}${a}!`
+    }
+    return inner 
+}
+
+// wrapAdjective ("||")( "a dedicated programmer")
